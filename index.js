a53850b6478dfb4fbcb09eaa9501495cf12ce535
Nicholas Kinsey <pyro@feisty.io>
2016-02-18 15:49:12 +1000

    Add out parameter to avoid GC garbage
    Fixes #1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var lerp = require('lerp-array');
 
-function interpolateArrays(arrays, t) {
+function interpolateArrays(arrays, t, out) {
     if (t >= 1) {
         return arrays[arrays.length - 1];
     }
@@ -11,7 +11,7 @@ function interpolateArrays(arrays, t) {
     var stopF = t * numStops;
     var stop = Math.floor(stopF);
     var k = stopF - stop;
-    return lerp(arrays[stop], arrays[stop+1], k);
+    return lerp(arrays[stop], arrays[stop+1], k, out);
 }
 
 module.exports = interpolateArrays;
