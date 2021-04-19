47ca7b1c9145676c23cd31737447517b02cf03d2
Marcin Ignac <marcin.ignac@gmail.com>
2021-04-19 11:43:01 +0100

    Add out param support for edge cases

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var lerp = require('lerp-array');
 
+function copyArrayTo(array, out) {
+    for (let i = 0; i < array.length; i++) {
+        out[i] = array[i]
+    }
+    return out
+}
+
 function interpolateArrays(arrays, t, out) {
     if (t >= 1) {
-        return arrays[arrays.length - 1];
+        const lastItem = arrays[arrays.length - 1];
+        return out ? copyArrayTo(lastItem, out) : lastItem
     }
     if (arrays.length == 1) {
-        return arrays[0];
+        const firstItem = arrays[0]
+        return out ? copyArrayTo(firstItem, out) : firstItem;
     }
     var numStops = arrays.length - 1;
     var stopF = t * numStops;
